33d918d099f491a8e190eae36ad4ddaab51e9619
Jorge Murillo <jorge.murillo.ext@itau.co>
2021-12-23 15:39:28 -0500

    se adiciona single-spa-angular y cambios basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'child2-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
